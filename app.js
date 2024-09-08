2ee3c45a21379e5a5ab50105af74be7bef378fc9
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-09-07 21:29:04 -0300

    Aula2

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const input = document.querySelector("input");
 const button = document.querySelector("button");
-
+const resultados = document.querySelector(".resultados-pesquisa");
 
 button.addEventListener("click", () => {
+    resultados.innerHTML = "";
     const value = input.value;
     alert(value);
 });
